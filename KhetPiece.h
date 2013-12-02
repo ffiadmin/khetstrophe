1437e20532260a549a5a54b72d82802f7f03c1f9
nicktallents <nicktallents11@gmail.com>
2013-12-02 15:31:00 -0500

    updated khetPiece

--- KhetPiece.h
+++ KhetPiece.h
@@ -8,8 +8,15 @@ class KhetPiece : public Tile
 public:
 	void rotate(int dir); // 0 left (-90 degree) rotation, 1 right (90 degree) rotation
 	virtual void onCollision()=0; // what to do when piece is hit by laser
+	
+	int getOrientation() {return orientation;}
+
+	char getColor() {return color;}
+	void setColor(char c) {color = c;}
+
 private:
 	int orientation; //current frame: 0 left, 1 up, 2 right, 3 down
+	char color; // r: red g: grey
 };
 
 class Invalid_rotation_dir_argument {};
