70081f90c0074579979befc456f96cb0969b25cd
Sariander <garrett.young17@gmail.com>
2013-12-04 16:45:03 -0500

    Scarab and Pyramid changes. Picture fixes for Zach

--- Scarab.h
+++ Scarab.h
@@ -13,73 +13,39 @@ public:
 		switch (Scarab::getOrientation())
 		{
 		case 0:
-			switch (dir)
-			{
-			case 0:
-				return DOWN;
-				break;
-			case 1:
-				return RIGHT;
-				break;
-			case 2:
-				return UP;
-				break;
-			case 3:
-				return LEFT;
-				break;
-			}
-			break;
-		case 1:
-			switch (dir)
-			{
-			case 0:
-				return UP;
-				break;
-			case 1:
-				return LEFT;
-				break;
-			case 2:
-				return DOWN;
-				break;
-			case 3:
-				return RIGHT;
-				break;
-			}
-			break;
 		case 2:
 			switch (dir)
 			{
 			case 0:
 				return DOWN;
-				break;
 			case 1:
 				return RIGHT;
-				break;
 			case 2:
 				return UP;
-				break;
 			case 3:
 				return LEFT;
-				break;
+			default:
+				return NOTHING;
 			}
 			break;
+		case 1:
 		case 3:
 			switch (dir)
 			{
 			case 0:
 				return UP;
-				break;
 			case 1:
 				return LEFT;
-				break;
 			case 2:
 				return DOWN;
-				break;
 			case 3:
 				return RIGHT;
-				break;
+			default:
+				return NOTHING;
 			}
 			break;
+		default:
+			return NOTHING;
 		}
 	}
 private:
