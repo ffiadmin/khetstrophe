df8299ea3c92b96bf0c5da3fe8731ac416fb1634
nicktallents <nicktallents11@gmail.com>
2013-12-04 19:19:27 -0500

    created Khet class

---
+++ Khet.h
@@ -0,0 +1,32 @@
+#ifndef KHET_H             // Prevent multiple definitions if this 
+#define KHET_H             // file is included in more than one place
+#define WIN32_LEAN_AND_MEAN
+
+#include "game.h"
+
+
+class Khet : public Game
+{
+private:
+
+
+public:
+    // Constructor
+    Khet();
+    // Destructor
+    virtual ~Khet();
+    // Initialize the game
+    void initialize(HWND hwnd);
+
+    void update();      // must override pure virtual from Game
+    void ai();          // "
+    void collisions();  // "
+    void render();      // "
+
+    void releaseAll();
+    void resetAll();
+
+	void restart();
+};
+
+#endif
